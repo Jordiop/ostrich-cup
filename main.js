e85e5987dcdb0e266b45083c99a7a74f0b4413e1
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2023-02-05 19:31:40 +0100

    update: everything working

--- main.js
+++ main.js
@@ -1,29 +1,52 @@
 // Cream sa array de objectes ostriches;
 var interval;
+var numero;
+var randomizador;
 
 function printScreen() {
     const app = document.getElementById("app");
     app.innerHTML = "";
-    for (let i = 0; i < 4; i++) {
+    numero = document.getElementById("numero").value;
+    if (numero <= 0) {
+        numero = 1;
+    }
+    for (let i = 0; i < numero; i++) {
         let container = document.createElement("div");
-        container.innerHTML = '<div class="flex"><img id="ostrich' + i + '" src="ostrich.gif" class="ostrich relative h-[8rem]" alt="ostrich"></div>'
+        container.innerHTML = '<div class="flex">' + i + '<img id="ostrich' + i + '" src="ostrich.gif" class="ostrich relative h-[8rem]" alt="ostrich"></div>'
         app.appendChild(container);
     }
     ostrichesList = document.getElementsByClassName("ostrich");
 }
 
 function race() {
-    for (let i = 0; i < ostrichesList.length; i++) {
-        if (ostrichesList[i].offsetLeft >= document.getElementById("app").offsetWidth - 50) {
-            console.log("Ganó la " + i);
-            reset();
-        } else {
-            let random = Math.floor(Math.random() * (10 - (1))) + (1);
-            ostrichesList[i].style.left = ostrichesList[i].offsetLeft + random + "px";
-            console.log(ostrichesList[i].offsetLeft);
-        }
+    let game = true;
+    let velocidad = document.getElementById("velocidad").value;
+    if (velocidad == 1) {
+        velocidad = 1;
+    } else if (velocidad == 2) {
+        velocidad = 2;
+    } else if (velocidad == 3) {
+        velocidad = 5;
+    }
+    console.log(velocidad)
+    ostrichesList = document.getElementsByClassName("ostrich");
+    randomizador = Math.floor(Math.random() * (numero));
+    if (ostrichesList[randomizador].offsetLeft == 600) {
+        console.log(ostrichesList[randomizador] + "murió")
+        ostrichesList[randomizador].remove();
+        ostrichesList = document.getElementsByClassName("ostrich");
+    } else if (ostrichesList[randomizador].offsetLeft >= document.getElementById("app").offsetWidth - 50) {
+        console.log("Ganó la " + randomizador);
+        reset();
+    } else {
+        let random = Math.floor(Math.random() * (velocidad - (0)));
+        ostrichesList[randomizador].style.left = ostrichesList[randomizador].offsetLeft + random + "px";
+        console.log(ostrichesList[randomizador].offsetLeft);
     }
 }
+
+
+
 
 function destroy() {
     for (let j = 0; j < ostrichesList.length; j++) {
@@ -39,5 +62,5 @@ function reset() {
 
 function update() {
     printScreen();
-    interval = setInterval(race, 70);
+    interval = setInterval(race, 10);
 }
